e16a25e695e3c613479f5980ef40a62039d396a2
m.vanwinsum <mitchell342@hotmail.com>
2018-12-06 16:55:19 +0100

    Completed D3 and setup D5

---
+++ 05_Day_Five/day_five_part_one.py
@@ -0,0 +1,7 @@
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+with open('puzzle_input.txt') as input_file:
+    instruction_list = [input_line.strip() for input_line in input_file]
+
+for input_line in instruction_list:
+    pass
