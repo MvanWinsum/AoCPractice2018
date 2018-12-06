4433aa946c7a37024182e250cb2263264b52e108
m.vanwinsum <mitchell342@hotmail.com>
2018-12-06 17:38:36 +0100

    Completed D5P1 (with some help from Reddit :O)

--- 05_Day_Five/day_five_part_one.py
+++ 05_Day_Five/day_five_part_one.py
@@ -1,7 +1,19 @@
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 with open('puzzle_input.txt') as input_file:
-    instruction_list = [input_line.strip() for input_line in input_file]
+    input_letters = [input_line.strip() for input_line in input_file][0]
 
-for input_line in instruction_list:
-    pass
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+pairs = [letter + letter.upper() for letter in alphabet]
+pairs += [letter.upper() + letter for letter in alphabet]
+
+
+while True:
+    og_string_len = len(input_letters)
+    for pair in pairs:
+        input_letters = input_letters.replace(pair, '')
+    if og_string_len == len(input_letters):
+        break
+
+
+pp.pprint(len(input_letters))
