10cde94161aa529d224e4195ef8e69dc007d3066
BloodlessMercy <mitchell342@hotmail.com>
2019-12-03 11:30:03 +0100

    AOC 2019: Day Three Part One

---
+++ AOC2019/03_Day_Three/03_part_one.py
@@ -0,0 +1,51 @@
+def walk_steps(steps):
+    x = 0
+    y = 0
+    crossed = []
+    for step in steps:
+        if step[0] == 'U':
+            for take_step in range(int(step[1:])):
+                y += 1
+                crossed.append(tuple([x, y]))
+        elif step[0] == 'D':
+            for take_step in range(int(step[1:])):
+                y -= 1
+                crossed.append(tuple([x, y]))
+        elif step[0] == 'R':
+            for take_step in range(int(step[1:])):
+                x += 1
+                crossed.append(tuple([x, y]))
+        elif step[0] == 'L':
+            for take_step in range(int(step[1:])):
+                x -= 1
+                crossed.append(tuple([x, y]))
+    return crossed
+
+
+def check_intersections(wire_a, wire_b):
+    # intersections = []
+    # for coordinate in wire_a:
+    #     if coordinate in wire_b:
+    #         print('Intersection: ' + coordinate)
+    #         intersections.append(coordinate)
+    return set(wire_a) & set(wire_b)
+
+
+def closest_intersection(intersections):
+    closest_distance = 0
+    for intersection in intersections:
+        intersection_distance = abs(intersection[0]) + abs(intersection[1])
+        if closest_distance == 0 or closest_distance > intersection_distance:
+            closest_distance = intersection_distance
+    return closest_distance
+
+
+with open('puzzle_input.txt') as input_file:
+    wires = [x.replace("\n", "").split(',') for x in input_file.readlines()]
+    print(len(wires))
+    wire_a = walk_steps(wires[0])
+    print(len(wire_a))
+    wire_b = walk_steps(wires[1])
+    intersections = check_intersections(wire_a, wire_b)
+print(intersections)
+print(closest_intersection(intersections))
