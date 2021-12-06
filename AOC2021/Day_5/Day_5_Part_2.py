2535cfe170d5dd77c73f76e56720de1c640dab8e
Mitchell van Winsum <mitchell.vanwinsum@olyslager.com>
2021-12-06 10:46:21 +0100

    AoC 2021 Day five completed!

---
+++ AOC2021/Day_5/Day_5_Part_2.py
@@ -0,0 +1,97 @@
+import pprint as pp
+printer = pp.PrettyPrinter(indent=4)
+
+with open('puzzle_input.txt') as input_file:
+    instruction_list = []
+    for input_line in input_file:
+        input_line = input_line.strip().split(' -> ')
+        input_line[0] = input_line[0].split(',')
+        input_line[1] = input_line[1].split(',')
+        instruction_list.append(input_line)
+
+
+def get_grid_dimensions(lines):
+    max_x = 0
+    max_y = 0
+    for line in lines:
+        if int(line[0][0]) > max_x:
+            max_x = int(line[0][0])
+        if int(line[1][0]) > max_x:
+            max_x = int(line[1][0])
+        if int(line[0][1]) > max_y:
+            max_y = int(line[0][1])
+        if int(line[1][1]) > max_y:
+            max_y = int(line[1][1])
+    return max_x+1, max_y+1
+
+
+def create_grid(x,y):
+    grid = []
+    for row in range(y):
+        grid.append([])
+        for column in range(x):
+            grid[row].append(0)
+    return grid
+
+
+def draw_straight_line(grid, line):
+    x_line = [int(line[0][0]), int(line[1][0])]
+    if x_line[0] == x_line[1]:
+        x_line = [x_line[0]]
+    else:
+        x_line.sort()
+        x_line = range(x_line[0], x_line[1]+1)
+    y_line = [int(line[0][1]), int(line[1][1])]
+    if y_line[0] == y_line[1]:
+        y_line = [y_line[0]]
+    else:
+        y_line.sort()
+        y_line = range(y_line[0], y_line[1]+1)
+    for x_coord in x_line:
+        for y_coord in y_line:
+            grid[y_coord][x_coord] += 1
+    return grid
+
+
+def draw_diagonal_line(grid, line):
+    x_line = [int(line[0][0]), int(line[1][0])]
+    y_line = [int(line[0][1]), int(line[1][1])]
+    if x_line[0] > x_line[1]:
+        x_step = -1
+        x_line[1] -= 1
+    else:
+        x_step = 1
+        x_line[1] += 1
+    if y_line[0] > y_line[1]:
+        y_step = -1
+        y_line[1] -= 1
+    else:
+        y_step = 1
+        y_line[1] += 1
+    x_iterator = x_line[0]
+    y_iterator = y_line[0]
+    while x_iterator != x_line[1] and y_iterator != y_line[1]:
+        grid[y_iterator][x_iterator] += 1
+        x_iterator += x_step
+        y_iterator += y_step
+    return grid
+
+
+def get_answer(grid):
+    more_than_two = 0
+    for row in grid:
+        for column in row:
+            if column > 1:
+                more_than_two += 1
+    return more_than_two
+
+
+grid_x, grid_y = get_grid_dimensions(instruction_list)
+grid = create_grid(grid_x, grid_y)
+for line in instruction_list:
+    if line[0][0] == line[1][0] or line[0][1] == line[1][1]:
+        grid = draw_straight_line(grid, line)
+    else:
+        grid = draw_diagonal_line(grid, line)
+
+print(get_answer(grid))
